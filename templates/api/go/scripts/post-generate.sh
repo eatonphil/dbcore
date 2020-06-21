1d1ff6316559259ddd7e0c2c56af223217db13d8
phil@eatonphil.com <phil@eatonphil.com>
2020-06-21 18:15:35 +0000

    Set indivudally

--- templates/api/go/scripts/post-generate.sh
+++ templates/api/go/scripts/post-generate.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-set -eu
+set -e
+set -u
+set -x
 
 go get golang.org/x/tools/cmd/goimports
-if [[ ! -f ../go.mod ]]; then
+if [ ! -f ../go.mod ]; then
     (cd .. && go mod init {{ api.extra.repo }})
 fi
 goimports -w ./

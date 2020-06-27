85b41d5cd1c7503c3499a396a844779c401fff0d
phil@eatonphil.com <phil@eatonphil.com>
2020-06-27 18:09:52 +0000

    Fix logic

--- templates/browser/react/src/main.tsx
+++ templates/browser/react/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
   Switch,
-  {{~ if browser.default_route == '' ~}}
+  {{~ if browser.default_route != '' ~}}
   Redirect,
   {{~ end ~}}
   Route,
